df49f84279662ac2ed30b5642dd55b524d6f0918
djfm <fm.de.jouvencel@gmail.com>
2016-07-25 19:56:49 +0200

    Tests descriptions are more expresive.

--- test/monads.js
+++ test/monads.js
@@ -6,29 +6,32 @@ const {
   listMonad,
 } = require('../lib/monads');
 
-const mulBy2 = list => withMonad(listMonad)(
-  ({ inject }) => inject(x => x * 2)(list)
-);
+describe('Using Monads', () => {
+  describe('the list monad', () => {
+    describe('is convenient to map a function over a list', () => {
+      const mulBy2 = list => withMonad(listMonad)(
+        ({ inject }) => inject(x => x * 2)(list)
+      );
 
-describe('"mulBy2"', () => {
-  specify('[1, 2] => [2, 4]', () =>
-    mulBy2([1, 2]).should.deep.equal([2, 4])
-  );
-});
+      specify('[1, 2] x 2 => [2, 4]', () =>
+        mulBy2([1, 2]).should.deep.equal([2, 4])
+      );
+    });
 
+    describe('makes computing a cartesian product of several lists trivial', () => {
+      const cartesianProduct = (listA, listB) =>
+        withMonad(listMonad)(
+          ({ inject }) => inject(
+            (a, b) => ({ a, b })
+          )(listA, listB)
+        )
+      ;
 
-const cartesianProduct = (listA, listB) =>
-  withMonad(listMonad)(
-    ({ inject }) => inject(
-      (a, b) => ({ a, b })
-    )(listA, listB)
-  )
-;
-
-describe('"cartesianProduct"', () => {
-  specify('[1, 2] x [3, 4] = [[1, 3], [1, 4], [2, 3], [2, 4]]', () =>
-    cartesianProduct([1, 2], [3, 4]).should.deep.equal(
-      [{ a: 1, b: 3 }, { a: 1, b: 4 }, { a: 2, b: 3 }, { a: 2, b: 4 }]
-    )
-  );
+      specify('[1, 2] x [3, 4] = [[1, 3], [1, 4], [2, 3], [2, 4]]', () =>
+        cartesianProduct([1, 2], [3, 4]).should.deep.equal(
+          [{ a: 1, b: 3 }, { a: 1, b: 4 }, { a: 2, b: 3 }, { a: 2, b: 4 }]
+        )
+      );
+    });
+  });
 });
