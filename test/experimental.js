1769246d7df9d446c53874c1b41618575b50c846
djfm <fm.de.jouvencel@gmail.com>
2016-07-28 14:39:19 +0200

    I think the tax monad is proven to be a monad.

--- test/experimental.js
+++ test/experimental.js
@@ -22,13 +22,55 @@ const makeTaxMonad = taxRate => {
   };
 
   const taxExcludedMonad = {
+    // make :: Number -> TaxExcl Number
     make: taxExclPrice,
+
+    // bind :: TaxExcl Number -> (Number -> TaxExcl Number) -> TaxExcl Number
     bind: price => priceReturningFunction =>
       priceReturningFunction(
         ('taxIncl' in price) ?
           price.taxIncl / (1 + taxRate) :
           price.taxExcl
       ),
+
+    /**
+     * Proof of monad laws:
+     * 1) Left Identity: bind(make(x), f) = f(x)
+     *
+     * bind(make(x), f) = f(('taxIncl' in make(x)) ? something : make(x).taxExcl)
+     *                  = f(make(x).taxExcl)
+     *                  = f({ taxExcl: x }.taxExcl)
+     *                  = f(x)
+     *
+     *
+     * 2) Right Identity: bind(m, make) = m
+     *
+     * bind(m, make) = make(('taxIncl' in m) ? convert(m.taxIncl) : m.taxExcl)
+     *               = make(('taxIncl' in m) ? m.taxExcl : m.taxExcl)
+     *               = make(m.taxExcl)
+     *               = { taxExcl: m.taxExcl }
+     *
+     *
+     * 3) Associativity: bind(bind(m, f), g) = bind(m, x => bind(make(x), g))
+     *
+     * bind(bind(m, f), g) = g(('taxIncl' in bind(m, f)) ?
+     *                              convert(bind(m, f).taxIncl) :
+     *                              bind(m, f).taxExcl)
+     *                         )
+     *                     = g(bind(m, f).taxExcl)
+     *                     = g(f(('taxIncl' in m ? convert(m.taxIncl) : m.taxExcl)).taxExcl)
+     *                     = g(f(m.taxExcl).taxExcl)
+     *                     := A
+     *
+     * bind(m, x => bind(f(x), g))
+     *     = (x => bind(f(x), g))(('taxIncl' in m) ? convert(m.taxIncl) : m.taxExcl)
+     *     = (x => bind(f(x), g))(m.taxExcl)
+     *     = bind(f(m.taxExcl), g)
+     *     = g(('taxIncl' in f(m.taxExcl)) ? convert(f(m.taxExcl).taxIncl) : f(m.taxExcl).taxExcl)
+     *     = g(f(m.taxExcl).taxExcl)
+     *     = A
+     *     = bind(bind(m, f), g) [QED]
+     */
   };
 
   const taxIncludedDo = inject(taxIncludedMonad);
