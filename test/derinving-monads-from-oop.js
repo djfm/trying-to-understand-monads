d559f54b82a3eb0133eb685561ec0b23e4c07275
djfm <fm.de.jouvencel@gmail.com>
2016-07-29 08:29:32 +0200

    WIP

--- test/derinving-monads-from-oop.js
+++ test/derinving-monads-from-oop.js
@@ -1,5 +1,5 @@
 describe('Deriving Monads from OOP', () => {
-  context('I have a simple vector class that can add another vector...', () => {
+  context('I have a simple vector class that can "add" and "sub" another vector...', () => {
     class Vector {
       constructor(x, y) {
         this.x = x;
@@ -12,6 +12,13 @@ describe('Deriving Monads from OOP', () => {
           this.y + otherVector.y
         );
       }
+
+      sub(otherVector) {
+        return new Vector(
+          this.x - otherVector.x,
+          this.y - otherVector.y
+        );
+      }
     }
 
     specify('it supports a fluent API, I can do vector.add(otherVector)', () => {
@@ -21,25 +28,56 @@ describe('Deriving Monads from OOP', () => {
       });
     });
 
-    specify('...but hell, my API produced a list of vectors that I need to sum!', () => {
-      class SmarterVector extends Vector {
-        static add(firstVector, ...nextVectors) {
-          return nextVectors.reduce(
-            (sumSoFar, vectorToAdd) => sumSoFar.add(vectorToAdd),
-            firstVector
-          );
+    specify('it supports a fluent API, I can do vector.sub(otherVector)', () => {
+      new Vector(1, 2).sub(new Vector(3, 4)).should.deep.equal({
+        x: -2,
+        y: -2,
+      });
+    });
+
+    context('...but hell, my API produced a list of vectors...', () => {
+      specify('...and I need to sum them all!', () => {
+        class SmarterVector extends Vector {
+          static add(firstVector, ...nextVectors) {
+            return nextVectors.reduce(
+              (sumSoFar, vectorToAdd) => sumSoFar.add(vectorToAdd),
+              firstVector
+            );
+          }
         }
-      }
 
-      const vectors = [
-        new Vector(1, 2),
-        new Vector(3, 4),
-        new Vector(5, 6),
-      ];
+        const vectors = [
+          new Vector(1, 2),
+          new Vector(3, 4),
+          new Vector(5, 6),
+        ];
 
-      SmarterVector.add(...vectors).should.deep.equal({
-        x: 9,
-        y: 12,
+        SmarterVector.add(...vectors).should.deep.equal({
+          x: 9,
+          y: 12,
+        });
+      });
+
+      specify('...and I need to sub them all for some reason!', () => {
+        class SmarterVector extends Vector {
+          static add(firstVector, ...nextVectors) {
+            return nextVectors.reduce(
+              (sumSoFar, vectorToAdd) => sumSoFar.add(vectorToAdd),
+              firstVector
+            );
+          }
+        }
+
+        const vectors = [
+          new Vector(1, 2),
+          new Vector(3, 4),
+          new Vector(5, 6),
+        ];
+
+        SmarterVector.add(...vectors).should.deep.equal({
+          x: 9,
+          y: 12,
+        });
       });
     });
   });
